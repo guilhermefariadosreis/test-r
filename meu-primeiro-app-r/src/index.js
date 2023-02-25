31de3a866d87bcf7afe6499ed115cf3917531c87
Guilherme Faria dos Reis <guilherme.fr@hotmail.com>
2023-02-25 02:47:44 +0000

    apostila03: fim da apostila

---
+++ meu-primeiro-app-r/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+const App = () => {
+    const estilosBotao = {marginTop: 12, paddingTop: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+    const textoDoRotulo = 'Nome:'
+    const obterTextoDoBotao = () => 'Enviar'
+    const testeClique = () => alert("Clicou...")
+    return (
+        <div 
+        style={{width: 768, margin: 'auto', backgroundColor: '#EEE', padding: 12, borderRadius: 8 }}>
+        <label
+            className='rotulo' 
+            htmlFor='nome'
+            style={{display: 'block', marginBottom: 4}}>
+                {textoDoRotulo}
+            </label>
+            <input 
+            type='text' 
+            id='nome' 
+            style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}}>
+            </input>
+            <button 
+            onClick = {() => testeClique()}
+            id="enviar" 
+            style={estilosBotao}>
+                {obterTextoDoBotao()}
+                </button>
+        
+        </div>
+    )
+} //arrow function
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
+
